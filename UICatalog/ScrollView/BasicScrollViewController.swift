ccbd2a9c885eb4d9c2d5576e406e329639c6d9c0
imk2o <imk2o@icloud.com>
2016-12-23 11:39:08 +0900

    Basic Scroll View追加

---
+++ UICatalog/ScrollView/BasicScrollViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BasicScrollViewController.swift
+//  UICatalog
+//
+//  Created by k2o on 2016/12/23.
+//  Copyright © 2016年 Yuichi Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class BasicScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Scroll ViewのAuto Layoutをうまく機能させるコツはStoryboardを参照
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
