35e01d93603f2b289d75339c5957ab2d6a86fc47
imk2o <imk2o@icloud.com>
2017-04-29 10:17:16 +0900

    アイコン追加、ImagePicker調整

--- UICatalog/ImagePicker/ImagePickerViewController.swift
+++ UICatalog/ImagePicker/ImagePickerViewController.swift
@@ -16,15 +16,12 @@ class ImagePickerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 画面表示後、自動でアクションシートを表示
+        DispatchQueue.main.async {
+            self.proposeAction()
+        }
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        self.proposeAction()
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
