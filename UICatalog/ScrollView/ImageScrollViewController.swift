b96a65718e6b501cb0c2fb2be63d60b1dc1abedb
imk2o <imk2o@icloud.com>
2022-02-12 16:40:16 +0900

    ImageScrollView: 可視範囲を画像として保存できるように

--- UICatalog/ScrollView/ImageScrollViewController.swift
+++ UICatalog/ScrollView/ImageScrollViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ImageScrollViewController: UIViewController {
 
@@ -19,17 +20,45 @@ class ImageScrollViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // https://qiita.com/takehilo/items/17ee84059332650a21a7
 
-    /*
-    // MARK: - Navigation
+    @IBAction func crop(_ sender: Any) {
+        guard let image = imageView.image else { return }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // [1]
+        let cropAreaRect = scrollView.bounds
+
+        // [2]
+        let imageViewScale = max(
+            (image.size.width * image.scale) / imageView.frame.width,
+            (image.size.height * image.scale) / imageView.frame.height
+        )
+
+        // [3]
+        let imageOriginInImageView = AVMakeRect(aspectRatio: image.size, insideRect: imageView.frame).origin
+
+        // [4]
+        let cropZone = CGRect(
+            x: (cropAreaRect.origin.x - imageOriginInImageView.x) * imageViewScale,
+            y: (cropAreaRect.origin.y - imageOriginInImageView.y) * imageViewScale,
+            width: cropAreaRect.width * imageViewScale,
+            height: cropAreaRect.height * imageViewScale
+        )
+
+        guard let croppedCGImage = image.cgImage?.cropping(to: cropZone) else { return }
+
+        // [5]
+        share(image: UIImage(cgImage: croppedCGImage))
     }
-    */
+    
+    private func share(image: UIImage) {
+        let activityViewController = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
 
+        present(activityViewController, animated: true, completion: nil)
+    }
 }
 
 extension ImageScrollViewController: UIScrollViewDelegate {
